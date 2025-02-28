449f45ac36cfa32352db06109940cc1c616de554
Chauhan Vivek Singh <105410640+chauhanviveksingh@users.noreply.github.com>
2025-02-28 22:10:02 +0530

    Initial commit

---
+++ Day_23/Day_23.java
@@ -0,0 +1,18 @@
+package Day_23;
+
+public class Day_23 {
+    
+
+
+    // Constructor overloading
+    // parametralized Constructor
+
+
+    // Reversive constructor is not allowed
+
+
+    // static variables vs Normal variables
+
+    // static block
+}
+
---
+++ Day_23/constructor_class.java
@@ -0,0 +1,17 @@
+package Day_23;
+
+public class constructor_class {
+    public static void main(String args[]) {
+
+
+
+        class Mock{
+            int age;
+            String name;
+            Mock(int age, String name){
+                
+            }
+        }
+    }
+    
+}
